f9f493140b4c0992e9458a232518b31e80fe273e
Asya <aya29@case.edu>
2024-09-04 22:15:14 -0400

    plot fixed but accuracy score still not working

--- Homework_One/pimaPerceptron.py
+++ Homework_One/pimaPerceptron.py
@@ -1,5 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
 
 '''
 Train a Perceptron to the entire Pima Indians data set (available in the Files > Data Sets section on
@@ -7,7 +10,7 @@ Canvas). Do not split the data into training and test for this problem!
 a) Experiment with different learning rates and report the highest (training set) classification
 accuracy you can obtain.
 
-The highest classification accuracy came from some of the lowest learning rates. 
+The highest classification accuracy came from some of the lowest learning rates, like 0.001 for example. 
 
 b) For the learning rate that gives the highest classification accuracy, plot the number of mis-
 classification errors against the number of epochs, similar to Figure 2.7 from the textbook
@@ -21,30 +24,31 @@ Submit your code in a file named pimaPerceptron.py.
 random_state = 1
 class Perceptron:
 
-    def __init__(self, eta=0.0001, n_iter=50, random_state=1):
-        self.eta = eta
-        self.n_iter = n_iter
+    def __init__(self, learning_rate=1, iterations=150, random_state=1):
+        self.learning_rate = learning_rate
+        self.iterations = iterations
         self.random_state = random_state
     
-    def fit(self, X, y):
-        rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
-        self.b_ = np.float64(0.)
-        self.errors_ = []
-        self.predictions_ = [] # Added by Kevin
+    def fit(self, x, y):
+        weight_init = np.random.RandomState(self.random_state)
+        self.weight = weight_init.normal(loc=0.0, scale=0.01, size=x.shape[1] + 1)
+        self.bias = np.float64(0.)
+        self.errors = []
+        self.predictions = [] # Added by Kevin
 
-        for i in range(self.n_iter):
+        for i in range(self.iterations):
             errors = 0
             for xi, target in zip(X, y):
-                update = self.eta * (target - self.predict(xi))
-                self.w_ += update * xi
-                self.b_ += update
+                update = self.learning_rate * (target - self.predict(xi))
+                self.weight += update * xi
+                self.bias += update
                 errors += int(update != 0.0)
-            self.errors_.append(errors)
-            self.predictions_.append(self.predict(X)) # Added by Kevin
+            self.errors.append(errors)
+            self.predictions.append(self.predict(X)) # Added by Kevin
         return self
+    
     def net_input(self, X):
-        return np.dot(X, self.w_) + self.b_
+        return np.dot(X, self.weight) + self.bias
     def predict(self, X):
         return np.where(self.net_input(X) >= 0.0, 1, 0)
     
@@ -53,14 +57,27 @@ class Perceptron:
 #turn csv file into np array 
 X = np.genfromtxt('C:/Machine Learning/pima-indians-diabetes.csv', delimiter=',', skip_header=1)
 
+#features
+features = X[:, :-1]
+print(np.shape(features))
+
 #target values 
 target_values = X[:, -1]
-ppn_pima = Perceptron(eta=0.9, n_iter=10, random_state=random_state)
-ppn_pima.fit(X, target_values)
-plt.plot(range(1, len(ppn_pima.errors_) + 1), ppn_pima.errors_, marker='o')
+print(np.shape(target_values))
+
+scaler = StandardScaler()
+
+x_scale = scaler.fit_transform(features)
+
+ppn_pima = Perceptron(learning_rate=0.9, iterations=150, random_state=random_state)
+ppn_pima.fit(x_scale, target_values)
+plt.plot(range(1, len(ppn_pima.errors) + 1), ppn_pima.errors, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Number of updates')
 plt.title('Pima data')
-print(*ppn_pima.predictions_, sep='\n')
 
 plt.show()
+
+predictions = ppn_pima.predict(x_scale)
+print(np.shape(predictions))
+accuracy = accuracy_score(target_values, predictions)
