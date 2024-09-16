6e9515b45c94a7484a0e3d27dbf4650d9759d748
Asya Akkus <aya29@case.edu>
2024-09-15 22:15:45 -0400

    start of problem 4

---
+++ Homework_Two/lr_svm_wine.py
@@ -0,0 +1,10 @@
+'''
+Fit a logistic regression and SVM to the wine data set. Try out different kernels for the SVM,
+tuning hyperparameters, such as the Gaussian kernel width, manually. In the same manner as
+Homework 1, first pre-process the data by standardizing each attribute, and then split the data
+50/50 into training and test sets using train_test_split() with random_state=1 to provide
+a fixed seed. Report the highest test set accuracy you are able to obtain for each classifier. Submit
+your code in a file named lr_svm_wine.py.
+'''
+
+
