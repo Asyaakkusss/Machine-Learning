d94a04fdef84f90c77533617f191a09842b56050
Asya Akkus <aya29@case.edu>
2024-09-29 22:54:10 -0400

    explanations completed

--- Homework_Three/knn_dimensions.py
+++ Homework_Three/knn_dimensions.py
@@ -18,10 +18,24 @@ susceptible to the curse of dimensionality. Submit your code as knn_dimensions.p
 Hint: Use the sklearn.neighbors.NearestNeighbors class to compute the 1-nearest neigh-
 bor distances. Use the scipy.spatial.distance.pdist() function to compute the pairwise
 distances between all data points.
+
+a) For the hypersphere, we see a decreasing trend. This means that the amount of points inside the 
+hypersphere decreases over time. The points move further away from the center which suggests a paucity
+of data around values that are deemed to be "expected" by the algorithm (in this case, the origin). It 
+also indicates that data points are far away from one another. This represents the curse of dimensionality. 
+
+b) For the ratio of of nearest neighbor distance to mean pairwise distance, it increases over time. This 
+indicates that the points become further and further apart from one another as the dimensions increase. This 
+represents the curse of dimensionality because it reflects the way in which "closeness" and "farness" lose their 
+meaning at higher dimensions, making it difficult for nearest neighbor algorithms to do their jobs well and draw 
+accurate conclusions. 
 '''
 
 import numpy as np 
 import matplotlib.pyplot as plt
+from sklearn import neighbors 
+from sklearn.neighbors import NearestNeighbors
+from scipy.spatial.distance import pdist
 
 #function for calculating data points within the unit hypersphere 
 def hypersphere(data):
@@ -33,10 +47,23 @@ def hypersphere(data):
     
     return fraction
 
+def distance_acrobatics(data): 
+    neighbors = NearestNeighbors(n_neighbors=2).fit(data)
+    distances, indices = neighbors.kneighbors(data)
+
+    nearest_neighbor = distances
+
+    pairwise_distance = pdist(data)
+
+    mean_nn = np.mean(nearest_neighbor)
+    mean_pd = np.mean(pairwise_distance)
+
+    return mean_nn/mean_pd 
 
 #generating data points in d dimensions 
 d = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 fraction = []
+mean_dist = []
 
 for i in d: 
     data = np.random.uniform(low=-1, high=1, size=(1000, i))
@@ -44,10 +71,27 @@ for i in d:
     fraction.append(hypersphere(data))
     i = i + 1
 
+for j in d: 
+    data = np.random.uniform(low=-1, high=1, size=(1000,i))
+
+    mean_dist.append(distance_acrobatics(data))
+    j = j + 1
+
 
 plt.plot(d, fraction)
 plt.title("Fraction of Datapoints within the Unit Hypersphere")
 plt.xlabel("Dimensions")
-plt.ylabel("Distance from Origin")
+plt.ylabel("Average Distance from Origin")
 plt.show()
+
+plt.plot(d, mean_dist)
+plt.title("How close a nearest neighbor is Relative to a Randomly Selected Data Point")
+plt.xlabel("Dimensions")
+plt.ylabel("Average Closeness to Neighbor")
+plt.show()
+
+plt.plot()
+'''
+TODO: Are parts a and b correct? 
+'''
 
