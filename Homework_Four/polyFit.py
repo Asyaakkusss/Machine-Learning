25e2f39988d5e05ad8a75546d7f98761a656ca52
Asya Akkus <aya29@case.edu>
2024-11-02 21:24:10 -0400

    part a complete

--- Homework_Four/polyFit.py
+++ Homework_Four/polyFit.py
@@ -12,3 +12,60 @@ function of 𝑑. Include axis labels and a legend in your plot. Explain the tre
 c) Create the same two plots using ridge regression (L2 penalty) with regularization strength
 𝛼 = 10−6 instead of OLS and compare the results with OLS.
 '''
+
+#imports
+import numpy as np 
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt 
+
+#open csv and put things into np array
+testPoly = np.genfromtxt('Homework_Four/testPoly.csv', delimiter=',', skip_header=1)
+trainPoly = np.genfromtxt('Homework_Four/trainPoly.csv', delimiter=',', skip_header=1)
+
+print(np.shape(testPoly))
+print(np.shape(trainPoly))
+
+testPoly_X = testPoly[:, 0].reshape(-1,1)
+testPoly_Y = testPoly[:, 1].reshape(-1,1)
+
+trainPoly_X = trainPoly[:, 0].reshape(-1,1)
+trainPoly_Y = trainPoly[:, 1].reshape(-1,1)
+
+#create arrays for test and train mean squared errors 
+arr_testmse = []
+arr_trainmse = []
+
+#training and testing 
+for deg in range(1,9): 
+    poly = PolynomialFeatures(deg);
+    train_poly = poly.fit_transform(trainPoly_X)
+    test_poly = poly.transform(testPoly_X)
+
+
+    model = LinearRegression(); 
+    model.fit(train_poly, trainPoly_Y)
+
+    #prediction step for train 
+    train_prediction = model.predict(train_poly)
+    train_meansqerr = mean_squared_error(trainPoly_Y, train_prediction)
+    arr_trainmse.append(train_meansqerr)
+
+    #prediction step for test 
+    test_prediction = model.predict(test_poly)
+    test_meansqerr = mean_squared_error(testPoly_Y, test_prediction)
+    arr_testmse.append(test_meansqerr)
+
+iterations_array = np.arange(1, 9)
+
+plt.plot(iterations_array, arr_trainmse, label="Training Data Mean Squared Error")
+plt.xlabel("Polynomial Degree")
+plt.ylabel("Mean Squared Error (MSE)")
+plt.title("Training Data Mean Squared Error with OLS")
+plt.show()
+plt.plot(iterations_array, arr_testmse, label ="Test Data Mean Squared Error")
+plt.xlabel("Polynomial Degree")
+plt.ylabel("Mean Squared Error (MSE)")
+plt.title("Test Data Mean Squared Error with OLS")
+plt.show()
