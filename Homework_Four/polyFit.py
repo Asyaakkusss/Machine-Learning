9241ec0725d283d19b471e6fab21109725ca36b2
Asya Akkus <aya29@case.edu>
2024-11-02 23:59:08 -0400

    progress on autompg regression so far

--- Homework_Four/polyFit.py
+++ Homework_Four/polyFit.py
@@ -7,15 +7,29 @@ target. Submit your code in a file named polyFit.py.
 a) Plot the mean squared error (MSE) for the training data and the test data on the same axes
 as a function of maximum degree 𝑑. Include axis labels and a legend in your plot. Explain
 the trend in the plot.
+
+The MSE of the training data goes down over time as the model increases in complexity with increasing 
+degrees and the training data is fitted better. The MSE of the test data initially goes down but then
+starts going back up as overfitting begins to occur at higher degrees and the model begins to be 
+unable to be generalized to the test data. 
+
 b) Plot the normalized squared magnitude of the weight vector ‖𝒘‖2/𝑑 on a log scale as
 function of 𝑑. Include axis labels and a legend in your plot. Explain the trend in the plot.
+
+As the number of dimensions increases, the normalized squared magnitude of the weight vector also 
+increases. This makes sense because if a higher degree is used, the model probably will require 
+larger weight values to fit the training data more closely. This is because higher-degree polynomials 
+will grow much faster than lower-degree ones, and also because polynomials that are higher-order
+tend to overfit more and need bigger weights to capture every little detail of the model, especially if 
+it is not a straightforward curve. 
+
 c) Create the same two plots using ridge regression (L2 penalty) with regularization strength
 𝛼 = 10−6 instead of OLS and compare the results with OLS.
 '''
 
 #imports
 import numpy as np 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt 
@@ -76,4 +90,6 @@ plt.ylabel("Normalized Squared Magnitude of Weight Vector")
 plt.yscale("log")
 plt.title("Normalized Squared Weight Norm with OLS")
 plt.show()
+
+
 
--- Homework_Four/regression_autompg.py
+++ Homework_Four/regression_autompg.py
@@ -1,5 +1,5 @@
 '''
-he Auto-Mpg data set (available in the Files > Data Sets section on Canvas) contains attributes
+The Auto-Mpg data set (available in the Files > Data Sets section on Canvas) contains attributes
 about a variety of cars, such as the number of cylinders, horsepower, and weight. It also contains
 information on the estimated city fuel economy in miles per gallon (mpg).
 Your task is to predict the mpg given the other attributes. Split the data 50/50 into training and test
@@ -7,7 +7,7 @@ sets using train_test_split() with random_state=1 to provide a fixed seed. Try t
 lowing regression models, tuning hyperparameters as necessary:
 • Elastic net penalized linear regression with polynomial features.
 • Support vector regression (both linear and nonlinear).
-• Random forest regression.
+• Random forest regression. 
 • K-nearest neighbors regression.
 Report the maximum test set 𝑅2 you are able to obtain with each regression model. Submit your
 code as regression_autompg.py.
@@ -16,4 +16,43 @@ value. You can ignore the column when loading the data into a numPy array by usi
 line:
 np.loadtxt('auto-mpg-missing-data-removed.txt', comments='"')
 This causes numPy to ignore all text inside double quotes.
+
+Answer:
+
+1. Random Forest Regression: 0.7025374494908679
+2. K-nearest neighbors regression: 0.43389041538710194
+3. SVR Linear: 0.4782529186714335
+4. SVR Gaussian: 0.004707850737426522
+5. SVR Polynomial: 
+6. Elastic Net: 
+
 '''
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.metrics import r2_score
+
+
+data = np.loadtxt('Homework_Four/auto-mpg-missing-data-removed.txt', comments='"')
+print(np.shape(data))
+
+features = data[:, :7]
+targets = data[:, -1]
+
+x_train, x_test, y_train, y_test = train_test_split(features, targets, test_size=0.5, random_state = 1)
+
+#model = RandomForestRegressor()
+#model = KNeighborsRegressor(n_neighbors=100)
+#model = SVR(kernel='rbf', C=10, gamma=0.1, epsilon=.001)
+#model = SVR(kernel='linear', C=0.001, gamma='auto', epsilon=0.04)
+model = SVR(kernel='poly', C=100, gamma='auto', degree=3, epsilon=.1, coef0=1)
+model.fit(x_train, y_train)
+
+prediction = model.predict(x_test)
+
+r_squared = r2_score(y_test, prediction)
+print(r_squared)
